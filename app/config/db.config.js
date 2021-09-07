6a883789f4a9d2eb0c3e2cd78b964fbe8cb99be9
Yogender Kulshrestha <yogenderkulshrestha0001@gmail.com>
2021-09-07 16:21:55 +0530

    update db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://intecyogi:mevnnodejs@intecyogi.id0iw.mongodb.net/intecyogi?retryWrites=true&w=majority"//"mongodb+srv://intecyogi:mevnnodejs@intecyogi.id0iw.mongodb.net/intecyogi"
+    url: "mongodb+srv://intecyogi:mevnnodejs@intecyogi.id0iw.mongodb.net/intecyogi?retryWrites=true&w=majority"
 };
-
